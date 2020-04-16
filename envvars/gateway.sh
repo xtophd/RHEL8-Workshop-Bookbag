e5020e2417b41d242fe754a020d8055d545ef6e2
root <root@rhv2.lab.linuxsoup.com>
2020-04-16 23:24:25 +0000

    trying terminal split and console

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -7,10 +7,12 @@
 #   split/2 - Split twice for three vertically stacked terminals
 #
 #export TERMINAL_TAB='split'
+export TERMINAL_TAB='split'
 
 # Disable OpenShift Console, this is not appropriate for Red Hat Summit as it
 # would point to the wrong cluster.
 #export CONSOLE_URL='http://0.0.0.0:10083'
+export CONSOLE_URL='https://169.47.188.166/'
 
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
