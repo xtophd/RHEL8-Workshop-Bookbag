6b62b9f579d2bb95f09e0696703b7a5674b189fd
root <root@rhv2.lab.linuxsoup.com>
2020-04-17 00:58:39 +0000

    test of console #2

--- envvars/gateway.sh
+++ envvars/gateway.sh
@@ -12,14 +12,13 @@ export TERMINAL_TAB='split'
 # Disable OpenShift Console, this is not appropriate for Red Hat Summit as it
 # would point to the wrong cluster.
 #export CONSOLE_URL='http://0.0.0.0:10083'
-export CONSOLE_URL='https://169.47.188.166:443/'
 
 # Enable the following line to disable the right-hand pane entirely.
 # Only use this if you have no terminal, no slides, and no console.
 #export DEFAULT_PAGE='workshop'
 
 # Flags to explicitly enable/disable specific components
-#ENABLE_CONSOLE='false'
+ENABLE_CONSOLE='true'
 #ENABLE_DASHBOARD='false'
 #ENABLE_SLIDES='false'
 #ENABLE_TERMINAL='false'
